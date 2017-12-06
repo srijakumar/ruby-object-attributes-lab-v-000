2e44154dc404fab36d7e1779e5496c49081681af
Srija S <srijakumar7@gmail.com>
2017-12-06 00:43:50 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,36 @@
+class Person
+
+  def name=(person_name)
+    name = person_name
+  end
+
+  def name
+    name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job)
+    job = job
+  end
+
+  def breed
+    breed
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+
+end
