f12e1770733b897089b290177426f529db07b80a
Srija S <srijakumar7@gmail.com>
2017-12-06 00:44:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog
+
+  def name=(dog_name)
+    name = dog_name
+  end
+
+  def name
+    name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    breed = dog_breed
+  end
+
+  def breed
+    breed
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+
+
+end
