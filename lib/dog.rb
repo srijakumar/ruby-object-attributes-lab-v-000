f073bdddfcbc8d9234512d95d5382fa36d486350
Srija S <srijakumar7@gmail.com>
2017-12-06 00:25:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,19 @@
 class Dog
 
-  def name
-    
+  def name = (dog_name)
+    name_of_the_dog = dog_name
   end
 
-  @name
-  
+  def name
+    name_of_the_dog    
+  end
+
+  def name = (dog_name)
+    @name_of_the_dog = dog_name
+  end
+
+  def name
+    @name_of_the_dog    
+  end
+
 end
